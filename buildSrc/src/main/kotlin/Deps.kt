9d2af1a457a85d5a6dd7282a187ba6d32af2d084
St2r <st2_r@qq.com>
2022-03-31 23:25:32 +0800

    [bugfix]: deps 更新

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,6 +1,8 @@
 object Deps {
     const val SpringStarterJDBC = "org.springframework.boot:spring-boot-starter-jdbc"
     const val SpringStarterWEB = "org.springframework.boot:spring-boot-starter-web"
+    const val SpringStarterSecurity = "org.springframework.boot:spring-boot-starter-security"
+    const val SpringStarterRedis = "org.springframework.boot:spring-boot-starter-data-redis"
 
     const val KotlinReflect = "org.jetbrains.kotlin:kotlin-reflect"
     const val KotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
@@ -8,4 +10,6 @@ object Deps {
     const val PostgreSQL = "org.postgresql:postgresql"
 
     const val Gson = "com.google.code.gson:gson"
+
+    const val MybatisSpringBootStarter = ""
 }
