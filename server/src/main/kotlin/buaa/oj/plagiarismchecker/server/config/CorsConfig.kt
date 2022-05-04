2e8777e7ebaa46b31db74dd39728a3a88ccc779b
St2r <st2_r@qq.com>
2022-05-04 21:00:18 +0800

    [bugfix]: cors

---
+++ server/src/main/kotlin/buaa/oj/plagiarismchecker/server/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package buaa.oj.plagiarismchecker.server.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowCredentials(true)
+            .allowedMethods("*")
+            .maxAge(3600)
+    }
+}
